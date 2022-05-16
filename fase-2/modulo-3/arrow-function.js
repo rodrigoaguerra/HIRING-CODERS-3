3db91b1e026a6714d3b3de8fd79af904d1eee642
Rodrigo Alves Guerra <orbital@rodrigoalvesguerra.com.br>
2022-05-16 18:42:58 -0300

    add modulo-3

---
+++ fase-2/modulo-3/arrow-function.js
@@ -0,0 +1,20 @@
+function soma(x,y) {
+    return x + y;
+}
+
+const multiplicacao = function(x,y) {
+    return x * y;
+}
+
+console.log(multiplicacao(5,5));
+
+const dividir = (x,y) => {
+    return x/y;
+}
+
+console.log(dividir(25,5));
+
+
+const objeto = () => ({ nome: 'JC', sobreNome: 'Bombardelli'}); 
+
+console.log(objeto());
