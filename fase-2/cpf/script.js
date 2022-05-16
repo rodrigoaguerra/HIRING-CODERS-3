0fa69773d5aa2680cbb77159594bb6f422151542
Rodrigo Alves Guerra <orbital@rodrigoalvesguerra.com.br>
2022-05-16 14:42:40 -0300

    add validador de cpf

--- fase-2/cpf/script.js
+++ fase-2/cpf/script.js
@@ -1,12 +1,42 @@
 console.log('Carregou o arquivo');
 
 function validaCPF(cpf) {
-    console.log(cpf.length);
+
     if(cpf.length !== 11) {
         return false;
     } else {
         var numeros = cpf.substring(0,9);
+        var digitos = cpf.substring(9);
         
+        var soma = 0;
+        for(var i = 10; i > 1; i--) {
+            soma = numeros.charAt(10 - i) * i;
+        }
+
+        console.log(soma);
+
+        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        // validação do primeiro digito
+        if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        soma = 0;
+
+        numeros = cpf.substring(0,10);
+
+        for(var k = 11; k > 1; k--) {
+            soma = numeros.charAt(11 -k) * k;   
+        }
+
+        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+        
+        // validação do segundo digito
+        if(resultado != digitos.charAt(1)) {
+            return false;
+        }
+
         return true;
     }
 }
@@ -14,6 +44,8 @@ function validaCPF(cpf) {
 function validacao() {
     console.log('iniciando validacao cpf');
     var cpf = document.getElementById('cpf_digitado').value
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     var resultadoValidacao = validaCPF(cpf);
 
