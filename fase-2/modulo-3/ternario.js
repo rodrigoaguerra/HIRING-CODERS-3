e7e70b806e9139c7f2b9ddd5019ab52c0dc0b80f
Rodrigo Alves Guerra <orbital@rodrigoalvesguerra.com.br>
2022-05-17 13:00:07 -0300

    javascript intermediario

---
+++ fase-2/modulo-3/ternario.js
@@ -0,0 +1,21 @@
+let isvalid = true;
+
+function verify(isvalid) {
+    if(isvalid) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(verify(isvalid));
+
+const result = (isvalid) ? true : false;
+
+console.log(result);
+
+let zero = 0;
+
+const numeroResultado = zero > 0 ? 0 : -1;
+
+console.log(numeroResultado);
